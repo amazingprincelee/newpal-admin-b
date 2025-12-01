9d40fa3f04ff3c82ba55e9d4d7f5f105263eef58
Prince Lee <prince_lee.nna85@ymail.com>
2025-12-01 09:47:19 +0100

    comment trick

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,27 +1,27 @@
-import jwt from "jsonwebtoken";
-import User from "../models/user.js";
+// import jwt from "jsonwebtoken";
+// import User from "../models/user.js";
 
-export const authenticate = async (req, res, next) => {
-  try {
-    const authHeader = req.headers.authorization;
+// export const authenticate = async (req, res, next) => {
+//   try {
+//     const authHeader = req.headers.authorization;
 
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return res.status(401).json({ message: "No token provided" });
-    }
+//     if (!authHeader || !authHeader.startsWith("Bearer ")) {
+//       return res.status(401).json({ message: "No token provided" });
+//     }
 
-    const token = authHeader.split(" ")[1];
+//     const token = authHeader.split(" ")[1];
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    const user = await User.findById(decoded.id).select("-password");
+//     const user = await User.findById(decoded.id).select("-password");
 
-    if (!user) {
-      return res.status(401).json({ message: "User not found" });
-    }
+//     if (!user) {
+//       return res.status(401).json({ message: "User not found" });
+//     }
 
-    req.user = user; 
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: "Invalid or expired token" });
-  }
-};
+//     req.user = user; 
+//     next();
+//   } catch (error) {
+//     return res.status(401).json({ message: "Invalid or expired token" });
+//   }
+// };
